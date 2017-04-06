914a2258fd35813008f46cbc094d002f9dfc9406
blackgooes <blackgooes@126.com>
2017-04-06 09:18:55 +0800

    login
    register and login

---
+++ src/com/model/User.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * @author blackagooes
+ *
+ */
+public class User extends Model<User>{
+	public static final User dao = new User();
+}
